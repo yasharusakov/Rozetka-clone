97c19bb20b09765e01402a107b14c0edb9bdecaa
rusakovyasha <rusakov.yasha@gmail.com>
2022-02-05 17:36:52 +0200

    +header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,19 @@
+import Header from '../Header';
+
 import '../../styles/style.scss';
+import './App.scss';
 
 function App() {
     return (
-        <div className=""></div>
+        <>
+            <div className="wrapper">
+                <Header/>
+                <div className="wrapper__container">
+                    <main>
+                    </main>
+                </div>
+            </div>
+        </>
     )
 }
 
