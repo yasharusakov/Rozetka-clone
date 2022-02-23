4c2a0edd572356e9c745b82274343eb5c394bc81
rusakovyasha <rusakov.yasha@gmail.com>
2022-02-23 21:18:18 +0200

    added products

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -3,9 +3,24 @@ import basket from '../../resources/svg/basket.svg';
 import './Basket.scss';
 
 function Basket() {
+
+    const items = [];
+
     return (
         <div className="basket">
-            
+            <div className="basket__container">
+                {
+                    items.length > 0 ? null
+                    :
+                    (
+                        <div className="basket__nothing">
+                            <img src={basket} alt="basket" />
+                            <div className="basket__nothing__title">Корзина пуста</div>
+                            <div className="basket__nothing__sub-title">Но это никогда не поздно исправить :)</div>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
